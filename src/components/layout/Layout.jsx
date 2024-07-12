64a7554fa286f61092680a456c77e42c0895e069
bitbucket ofc <pankajkumar199528@gmail.com>
2024-07-13 00:45:16 +0530

    removed unwanted code

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,14 +1,11 @@
 import React from "react";
-import Header from "../header/Header";
 import MainBody from "../body/MainBody";
-import Footer from "../footer/Footer";
+
 
 const Layout = () => {
     return(
         <>
-           {/* <Header /> */}
            <MainBody />
-           {/* <Footer /> */}
         </>
     );
 }
